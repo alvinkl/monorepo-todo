988298a862f38c68839f886a376e32ea3f21c5f0
Alvin Kristanto <alvink77@gmail.com>
2018-08-20 10:37:33 +0700

    added import to service

--- packages/datasource/src/interfaces/Todo/ITodoService.ts
+++ packages/datasource/src/interfaces/Todo/ITodoService.ts
@@ -1,3 +1,5 @@
+import TodoListItemStore from "../../stores/Todo/TodoListItemStore";
+
 export default interface ITodoService {
   addTodo(todo: TodoListItemStore);
   saveTodos(todos: TodoListItemStore[]);
